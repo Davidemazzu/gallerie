915b125e10132e592ca1c1554354c5254a53947c
Davidemazzu <95235667+Davidemazzu@users.noreply.github.com>
2025-05-27 10:00:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
+const CACHE_NAME = 'gallerie-cache-v2'; // nuova versione
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('gallerie-cache').then(cache => {
-      return cache.addAll(['index.html', 'manifest.json', 'icon.png']);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png'
+      ]);
     })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
 });
 
